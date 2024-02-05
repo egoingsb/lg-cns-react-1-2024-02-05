ff52b890ca5fde806f5c776f8bdeb218c3cd292e
egoinig <egoing@gmail.com>
2024-02-05 13:11:48 +0900

    이벤트의 이름은 낙타표기법을 따른다. onclick ->onClick
    값은 반드시 함수다.
    
    set함수는 이전 값을 기반으로 새로운 값이 만들어질 때는 callback function 을 사용해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
+import {useState} from 'react';
 function Counter({title, initValue}){
-  return <div>
-    <h1>{title}</h1>
-    <button>+</button> {initValue}
-  </div>
+  const valueState = useState(initValue);
+  const value = valueState[0];
+  const setValue = valueState[1];
+  function up(){
+    setValue(value + 1);
+    // callback function
+    // setValue(function(prevValue){
+    //   return prevValue + 1;
+    // });
+  }
+  return (
+    <div>
+      <h1>{title}</h1>
+      <button onClick={up}>+</button> {value}
+    </div>
+  )
 }
 function App() {
   return (
     <div>
       <Counter title="카운터" initValue={2}/>
-      <Counter title="불면증 카운터" initValue={4}/>
     </div>
   );
 }
