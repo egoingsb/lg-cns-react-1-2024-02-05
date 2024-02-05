6b5f54dfeb95effd42c48c51760f9995eeb78a30
egoinig <egoing@gmail.com>
2024-02-05 15:11:08 +0900

    map, for를 이용해서 목록을 생성하는 경우, react가 각각의 항목을 추적할 수 있게 key값을 제공해야 한다. key값은 목록 안에서 유일하면 된다.
    마땅한 식별자가 없는 경우 index 값을 쓴다.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import {useState} from 'react';
 function Counter({title, initValue}){
   const [value, setValue] = useState(initValue);
   const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([]);
+  console.log('history', history);
+  const up = ()=>{
+    const newValue = value+step;
+    setValue(newValue);
+    
+    // 잘못된 사례
+    // history.push(newValue);
+    // setHistory(history);
+
+    const newHistory = [...history];
+    newHistory.push(newValue);
+    setHistory(newHistory);
+  }
   return (
     <div>
       <h1>{title}</h1>
       <input type="number" value={step} onChange={(evt)=>{
         setStep(Number(evt.target.value));
       }} />
-      <button onClick={()=>setValue(value + step)}>+</button> {value}
+      <button onClick={up}>+</button> {value}
+      <ol>
+          {history.map((item,index)=>{
+            return <li key={index}>{item}</li>
+          })}
+      </ol>
     </div>
   )
 }
