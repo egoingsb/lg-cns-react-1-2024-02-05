73ebbb18a848606ea27097f81928f792c5ef054f
egoinig <egoing@gmail.com>
2024-02-05 10:49:56 +0900

    props는 컴포넌트의 입력값이다. 첫번째 인자에 객체의 속성으로 존재한다. 값을 출력할 때는 {}를 사용한다.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-function Counter(){
+function Counter(props){
   return <div>
-    <h1>Counter</h1>
+    <h1>{props.title}</h1>
     <button>+</button> 5
   </div>
 }
 function App() {
   return (
     <div>
-      <Counter />
+      <Counter title="카운터"/>
+      <Counter title="불면증 카운터"/>
     </div>
   );
 }
