fc5b07d6fba3faa7a27093cca9550bb18af66f69
egoinig <egoing@gmail.com>
2024-02-05 17:45:45 +0900

    Counter를 별도의 파일로 분할
    1. export가 있어야 다른 파일에서 import 할 수 있다.
    
    2. export가 없으면 private이다.
    
    3. default가 붙으면 import할 때 중괄호 없이, 아무 이름이나 사용할 수 있다.
    
    4. default가 없으면 중괄호안에 동일한 이름을 사용해야 한다.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,11 @@
 import './App.css';
-import {useState, useEffect} from 'react';
 import style from './App.module.css';
-console.log('style', style.layout);
-function Counter({title, initValue}){
-  const [value, setValue] = useState(initValue);
-  const [step, setStep] = useState(1);
-  const [history, setHistory] = useState([]);
-  useEffect(()=>{
-    fetch('http://localhost:9999/counter')
-    .then(resp=>resp.json())
-    .then(result=>{
-         setValue(result.value);
-    })
-  },[]);
- 
-  const up = ()=>{
-    const newValue = value+step;
-    setValue(newValue);
-
-    fetch('http://localhost:9999/counter/',{
-      method:'PUT',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify({value:newValue})
-    })
-      .then(resp=>resp.json())
-      .then(result=>{
-        console.log(result);
-      })
-
-    const newHistory = [...history];
-    newHistory.push(newValue);
-    setHistory(newHistory);
-  }
-  const h1Text = {color:'red', fontSize:'30px'}
-  return (
-    <div className={style.layout}>
-      <h1 style={h1Text}>{title}</h1>
-      <input type="number" value={step} onChange={(evt)=>{
-        setStep(Number(evt.target.value));
-      }} />
-      <button onClick={up} className="btn">+</button> {value}
-      <ol>
-          {history.map((item,index)=>{
-            return <li key={index}>{item}</li>
-          })}
-      </ol>
-    </div>
-  )
-}
+import MyCounter, { Counter2 } from './Counter';
 function App() {
   return (
     <div>
-      <Counter title="카운터" initValue={2}/>
+      <MyCounter title="카운터" initValue={2}/>
+      <Counter2 />
     </div>
   );
 }
---
+++ src/Counter.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react';
+import style from './App.module.css';
+
+export function Counter2(){
+  return <div>Counter2</div>
+}
+
+export default function Counter({ title, initValue }) {
+  const [value, setValue] = useState(initValue);
+  const [step, setStep] = useState(1);
+  const [history, setHistory] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:9999/counter')
+      .then(resp => resp.json())
+      .then(result => {
+        setValue(result.value);
+      });
+  }, []);
+
+  const up = () => {
+    const newValue = value + step;
+    setValue(newValue);
+
+    fetch('http://localhost:9999/counter/', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ value: newValue })
+    })
+      .then(resp => resp.json())
+      .then(result => {
+        console.log(result);
+      });
+
+    const newHistory = [...history];
+    newHistory.push(newValue);
+    setHistory(newHistory);
+  };
+  const h1Text = { color: 'red', fontSize: '30px' };
+  return (
+    <div className={style.layout}>
+      <h1 style={h1Text}>{title}</h1>
+      <input type="number" value={step} onChange={(evt) => {
+        setStep(Number(evt.target.value));
+      }} />
+      <button onClick={up} className="btn">+</button> {value}
+      <ol>
+        {history.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ol>
+    </div>
+  );
+}
