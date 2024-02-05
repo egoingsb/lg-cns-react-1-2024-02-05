7a7a0387e934a4c42b30cd65fa64e84aab1b5821
egoinig <egoing@gmail.com>
2024-02-05 13:17:06 +0900

    익명함수

--- src/App.js
+++ src/App.js
@@ -4,17 +4,13 @@ function Counter({title, initValue}){
   const valueState = useState(initValue);
   const value = valueState[0];
   const setValue = valueState[1];
-  function up(){
-    setValue(value + 1);
-    // callback function
-    // setValue(function(prevValue){
-    //   return prevValue + 1;
-    // });
-  }
+  
   return (
     <div>
       <h1>{title}</h1>
-      <button onClick={up}>+</button> {value}
+      <button onClick={function (){
+        setValue(value + 1);
+      }}>+</button> {value}
     </div>
   )
 }
